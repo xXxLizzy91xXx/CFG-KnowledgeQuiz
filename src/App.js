9773ba933ed7238b00f155f2a6be42eaaa26192e
Elizabeth Muir <emuir91@hotmail.com>
2023-04-12 14:33:48 +0100

    Created a Navbar with active state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Navbar from './Navbar'
+import About from './Pages/about'
+import Home from './Pages/home'
+import Contact from './Pages/contact'
+import { Route, Routes} from 'react-router-dom'
 
 function App() {
+  
   return (
     <div className="App">
-      <h1>Unicorn World</h1>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
